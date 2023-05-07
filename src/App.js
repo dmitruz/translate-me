fbbf81d8cc8b5f37971c85ecefe6512769ea7f89
dmitruz <dmitruz970@gmail.com>
2023-05-07 21:31:49 +0300

    detect post method using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Form, TextArea, Button, Icon } from 'semantic-ui-react';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [detectedLanguageKey, setdetectedLanguageKey] = useState('');
+
+  const getLanguageSource = () => {
+    axios
+      .post(`https://libretranslate.de/detect`, {
+        q: inputText,
+      })
+      .then(response => {
+        setdetectedLanguageKey(response.data[0].language);
+      });
+  };
+
   return (
     <div>
       <div className="app-header">
@@ -11,15 +25,18 @@ export default function App() {
       <div className="app-body">
         <div>
           <Form>
-            <Form.Field control={TextArea} placeholder="Type Text to Translate.." />
+            <Form.Field
+              control={TextArea}
+              placeholder="Type Text to Translate.."
+              onChange={e => setInputText(e.target.value)}
+            />
 
             <select className="language-select">
               <option>Please Select Language..</option>
             </select>
-
             <Form.Field control={TextArea} placeholder="Your Result Translation.." />
 
-            <Button color="orange" size="large">
+            <Button color="orange" size="large" onClick={getLanguageSource}>
               <Icon name="translate" />
               Translate
             </Button>

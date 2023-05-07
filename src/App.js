8752aaa67cbbfd02c3dfae6a720206f2b909715e
dmitruz <dmitruz970@gmail.com>
2023-05-07 20:55:02 +0300

    Add basic styles

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Translate() {
+  return (
+    <div>
+      <div className="app-header">
+        <h2 className="header">Texty Translator</h2>
+      </div>
+    </div>
+  );
+}

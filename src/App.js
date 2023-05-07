7a85b7c229692bf2fdf95ea2f2b70538bbf5fa74
dmitruz <dmitruz970@gmail.com>
2023-05-07 21:01:41 +0300

    make murkup of app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Form, TextArea, Button, Icon } from 'semantic-ui-react';
 
-export default function Translate() {
+export default function App() {
   return (
     <div>
       <div className="app-header">
-        <h2 className="header">Texty Translator</h2>
+        <h2 className="header">Translate Me</h2>
+      </div>
+
+      <div className="app-body">
+        <div>
+          <Form>
+            <Form.Field control={TextArea} placeholder="Type Text to Translate.." />
+
+            <select className="language-select">
+              <option>Please Select Language..</option>
+            </select>
+
+            <Form.Field control={TextArea} placeholder="Your Result Translation.." />
+
+            <Button color="orange" size="large">
+              <Icon name="translate" />
+              Translate
+            </Button>
+          </Form>
+        </div>
       </div>
     </div>
   );
